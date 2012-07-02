3f088895a8cf90b1c92585e8b7262f52a32a5708
Jason Power <powerjg@cs.wisc.edu>
2012-07-02 17:04:13 -0500

    Added VI_hammer protocol VI_hammer uses the MOESI hammer protocol for CPU cache, between CPU and dir, and between GPU L2 and Dir GPU L1s are incoherent, but are write through (no write allocate)
    hg log -l 1Currently there is a deadlock when running with this protocolhg log -l 1

---
+++ configs/gpu_protocol/VI_hammer_fusion.py
@@ -0,0 +1,140 @@
+# Copyright (c) 2006-2007 The Regents of The University of Michigan
+# Copyright (c) 2009 Advanced Micro Devices, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met: redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer;
+# redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution;
+# neither the name of the copyright holders nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Authors: Brad Beckmann
+
+import math
+import m5
+from m5.objects import *
+from m5.defines import buildEnv
+
+#
+# Note: the L1 Cache latency is only used by the sequencer on fast path hits
+#
+class L1Cache(RubyCache):
+    latency = 3
+
+#
+# Note: the L2 Cache latency is not currently used
+#
+class L2Cache(RubyCache):
+    latency = 15
+
+def create_system(options, system, piobus, dma_devices, ruby_system):
+
+    if not buildEnv['GPGPU_SIM']:
+        m5.util.panic("This script requires GPGPU-Sim integration to be built.")
+
+    # Run the original protocol script
+    buildEnv['PROTOCOL'] = buildEnv['PROTOCOL'][:-7]
+    protocol = buildEnv['PROTOCOL']
+    exec "import %s" % protocol
+    try:
+        (cpu_sequencers, dir_cntrls, all_cntrls) = \
+            eval("%s.create_system(options, system, piobus, dma_devices, ruby_system)" % protocol)
+    except:
+        print "Error: could not create system for ruby protocol inside fusion system %s" % protocol
+        raise
+
+    #
+    # The ruby network creation expects the list of nodes in the system to be
+    # consistent with the NetDest list.  Therefore the l1 controller nodes must be
+    # listed before the directory nodes and directory nodes before dma nodes, etc.
+    #
+    l1_cntrl_nodes = []
+
+    #
+    # Caches for the stream processors
+    #
+    l2_bits = int(math.log(options.num_l2caches, 2))
+    block_size_bits = int(math.log(options.cacheline_size, 2))
+
+    cntrl_count = 0
+
+    for i in xrange(options.num_sc):
+        #
+        # First create the Ruby objects associated with this cpu
+        #
+        cache = L1Cache(size = options.sc_l1_size,
+                            assoc = options.sc_l1_assoc,
+                            replacement_policy = "LRU",
+                            start_index_bit = block_size_bits)
+
+        l1_cntrl = L1CacheVI_Controller(version = options.num_cpus + i,
+                                      cntrl_id = len(all_cntrls) + i,
+                                      cacheMemory = cache,
+                                      l2_select_num_bits = l2_bits,
+                                      ruby_system = ruby_system)
+
+        cpu_seq = RubySequencer(version = options.num_cpus + i,
+                                icache = cache,
+                                dcache = cache,
+                                access_phys_mem = True,
+                                ruby_system = ruby_system)
+
+        l1_cntrl.sequencer = cpu_seq
+
+        if piobus != None:
+            cpu_seq.pio_port = piobus.slave
+
+        exec("system.l1_cntrl_sp%02d = l1_cntrl" % i)
+
+        #
+        # Add controllers and sequencers to the appropriate lists
+        #
+        cpu_sequencers.append(cpu_seq)
+        l1_cntrl_nodes.append(l1_cntrl)
+
+        cntrl_count += 1
+
+    ######################################################################################
+    #copy engine cache (make as small as possible, ideally 0)
+    cache = L1Cache(size = "4096B", assoc = 2)
+
+    #
+    # Only one unified L1 cache exists.  Can cache instructions and data.
+    #
+    cpu_seq = RubySequencer(version = options.num_cpus+options.num_sc,
+                               icache = cache,
+                               dcache = cache,
+                               access_phys_mem = True,
+                               max_outstanding_requests=10, # This directly corresponds to the CE bandwidth
+                               ruby_system = ruby_system)
+
+    l1_cntrl = L1CacheCE_Controller(version = 0,
+                                    cntrl_id = len(all_cntrls)+options.num_sc,
+                                    sequencer = cpu_seq,
+                                    number_of_TBEs = 12,
+                                    ruby_system = ruby_system)
+
+
+    cpu_sequencers.append(cpu_seq)
+    l1_cntrl_nodes.append(l1_cntrl)
+
+    all_cntrls += l1_cntrl_nodes
+
+    return (cpu_sequencers, dir_cntrls, all_cntrls)
