acce503de1b4118d82821e743641f05da9ef26eb
Joel Hestness <jthestness@gmail.com>
2013-03-17 22:28:28 -0500

    Minor: se_fusion.py: Fix authorship

--- configs/se_fusion.py
+++ configs/se_fusion.py
@@ -25,7 +25,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
-# Authors: Steve Reinhardt
+# Authors: Jason Power, Joel Hestness
 
 import optparse
 import os
