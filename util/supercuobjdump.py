cd08aa5f0bc3111f91d3e04b595c73ed31d1cc20
Joel Hestness <jthestness@gmail.com>
2014-12-21 10:46:25 -0600

    Util: Add a cuobjdump utility
    Debugging GPU application memory bugs is very difficult in the simulator. Some
    things have been added to improve debugging, including DPRINTF functionality in
    ShaderLSQ, and printing the program counter on GPU page faults. This utility
    uses cuobjdump to print PTX code from a benchmark, and each line is prefixed
    with the PC of the instruction. For consistency, the PC defaults to printing in
    hex, but the --decimal option prints decimal PCs.

---
+++ util/supercuobjdump.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+
+import optparse
+import os
+import re
+from subprocess import *
+import sys
+
+def which(program):
+    import os
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
+
+def cleanEntryLine(line):
+    start = line.find('.entry ') + len('.entry ')
+    end = line.find(' ', start)
+    return line[start:end]
+
+def cleanVisibleFuncLine(line):
+    # Process lines of the form:
+    #  .visible .func _ZN14ComponentSpace6isBossEj
+    #  .visible .func (.param .s32 __cudaretf__ZN14ComponentSpace6isBossEj) _ZN14ComponentSpace6isBossEj
+    start = line.find('.visible .func ') + len('.visible .func ')
+    nextstart = line.find(') ', start)
+    if nextstart > 0:
+        start = nextstart + 2
+    end = line.find(' ', start)
+    return line[start:end]
+
+parser = optparse.OptionParser()
+parser.add_option("--decimal", action="store_true", default=False, help="Print PCs in decimal")
+(options, args) = parser.parse_args()
+
+if len(args) < 1:
+    print >>sys.stderr, 'ERROR: Must specify CUDA binary'
+    sys.exit(0)
+
+cubin = args[0]
+if not os.path.exists(cubin):
+    print >>sys.stderr, 'ERROR: CUDA binary (%s) does not exist' % cubin
+    sys.exit(0)
+
+if which('cuobjdump') is None:
+    print >>sys.stderr, 'ERROR: Must have cuobjdump in PATH variable'
+    sys.exit(0)
+
+process = Popen("cuobjdump -ptx %s" % cubin, shell=True, stdout=PIPE)
+output = process.communicate()[0]
+
+during_function = False
+last_func_line = ''
+pc = 0
+
+for line in output.split('\n'):
+    line = line.replace('\t','    ')
+    end = line.find('//')
+    if end < 0:
+        end = len(line)
+    line = line[0:end]
+    line = line.rstrip()
+
+    if '{' in line and '{%' not in line and '{_' not in line:
+        if during_function:
+            print line
+            assert(0)
+        if '.entry' in last_func_line:
+            print 'FUNCTION: %s' % cleanEntryLine(last_func_line)
+        elif '.visible .func' in last_func_line:
+            print 'FUNCTION: %s' % cleanVisibleFuncLine(last_func_line)
+        else:
+            print >>sys.stderr, 'ERROR: Strange function declaration'
+            sys.exit(0)
+        during_function = True
+
+    if '}' in line and '{%' not in line and '{_' not in line:
+        assert(during_function)
+        print '}'
+        print ''
+        during_function = False
+
+    if during_function:
+        if ('{' in line and '{%' not in line and '{%' not in line) or line == '':
+            print line.lstrip()
+        elif ':' in line or '.reg' in line or '.loc ' in line or '.local ' in line or '.shared ' in line or '.param ' in line:
+            print '        %s' % line.lstrip()
+        else:
+            if line[-1] != ';':
+                print line
+                assert(0)
+            if options.decimal:
+                pcstring = '%u:' % pc
+            else:
+                pcstring = '0x%x:' % pc
+            print '%-10s%s' % (pcstring, line)
+            pc += 8
+    else:
+        if '.func' in line or '.entry' in line:
+            last_func_line = line
+
