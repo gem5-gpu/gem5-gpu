1b8d98bd33f4930cd027dd3e2ac03db928111f16
Joel Hestness <jthestness@gmail.com>
2015-07-08 11:45:37 -0500

    supercuobjdump: Adjust code for debug binaries
    CUDA fat binaries built with -G option add numerous annotations that are
    printed by cuobjdump. Add handling for these annotations.

--- util/supercuobjdump.py
+++ util/supercuobjdump.py
@@ -40,6 +40,17 @@ def cleanVisibleFuncLine(line):
     end = line.find(' ', start)
     return line[start:end]
 
+def cleanFuncLine(line):
+    # Process lines of the form:
+    #  .func _ZN14ComponentSpace6isBossEj
+    #  .func (.param .s32 __cudaretf__ZN14ComponentSpace6isBossEj) _ZN14ComponentSpace6isBossEj
+    start = line.find('.func ') + len('.func ')
+    nextstart = line.find(') ', start)
+    if nextstart > 0:
+        start = nextstart + 2
+    end = line.find(' ', start)
+    return line[start:end]
+
 parser = optparse.OptionParser()
 parser.add_option("--debug", action="store_true", default=False, help="Debug print all lines")
 parser.add_option("--decimal", action="store_true", default=False, help="Print PCs in decimal")
@@ -62,6 +73,7 @@ process = Popen("cuobjdump -ptx %s" % cubin, shell=True, stdout=PIPE)
 output = process.communicate()[0]
 
 during_function = False
+during_section = False
 last_func_line = ''
 pc = 0
 
@@ -76,23 +88,30 @@ for line in output.split('\n'):
     line = line.rstrip()
 
     if '{' in line and '{%' not in line and '{_' not in line:
-        if during_function:
+        if during_function or during_section:
             print line
             assert(0)
-        if '.entry' in last_func_line:
-            print 'FUNCTION: %s' % cleanEntryLine(last_func_line)
-        elif '.visible .func' in last_func_line:
-            print 'FUNCTION: %s' % cleanVisibleFuncLine(last_func_line)
+        if '.section ' in line:
+            print 'SECTION: ',
+            during_section = True
         else:
-            print >>sys.stderr, 'ERROR: Strange function declaration'
-            sys.exit(0)
-        during_function = True
+            if '.entry' in last_func_line:
+                print 'FUNCTION: %s' % cleanEntryLine(last_func_line)
+            elif '.visible .func' in last_func_line:
+                print 'FUNCTION: %s' % cleanVisibleFuncLine(last_func_line)
+            elif '.func' in last_func_line:
+                print 'FUNCTION: %s' % cleanFuncLine(last_func_line)
+            else:
+                print >>sys.stderr, 'ERROR: Strange function declaration'
+                sys.exit(0)
+            during_function = True
 
     if '}' in line and '{%' not in line and '{_' not in line:
-        assert(during_function)
+        assert (during_function and not during_section) or (not during_function and during_section)
         print '}'
         print ''
         during_function = False
+        during_section = False
 
     if during_function:
         if ('{' in line and '{%' not in line and '{%' not in line) or line == '':
@@ -109,6 +128,8 @@ for line in output.split('\n'):
                 pcstring = '0x%x:' % pc
             print '%-10s%s' % (pcstring, line)
             pc += 8
+    elif during_section:
+        print '        %s' % line.lstrip()
     else:
         if '.func' in line or '.entry' in line:
             last_func_line = line
